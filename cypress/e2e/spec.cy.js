e8456d25d00ba71ddd9595d211f70a90da303bdb
Anastasia Strikou <anastasiastrikou@gmail.com>
2023-12-22 14:38:07 +0200

    Added test and fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,6 +7,14 @@ describe('contains logo', () => {
   })
 })
 
+describe('empty cart checkout', () => {
+  it('passes', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.cart').get('.cart-icon').click()
+//    cy.get('.cart').get('.cart-preview').get('.action-block').get('button').click()
+  })
+})
+
 // describe('succesfully add one product to cart', () => {
 //   it('passes', () => {
 //     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
